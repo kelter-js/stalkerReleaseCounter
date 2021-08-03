7743ab308398f8badf329bc15215f3a0eff7e06c
kelter-js <spawnkillerqw@gmail.com>
2021-08-03 21:36:57 +0400

    Добавляет подвал, правки разметки, файл переменных css

--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@ import React from 'react'
 import {render} from 'react-dom'
 import './sass/style.sass'
 
-function App () {
+function MainApp () {
     return (
-        <div className="container">
+        <div className='timer__wrapper'>
             <h1>Start</h1>
         </div>
     )
 }
 
-render (<App/>, document.querySelector('#root'));
+function FooterApp () {
+  return (
+    <div className='container'>
+        <p className='footer__copyright'>Все права принадлежат оригинальным правообладателям - GSC Game World</p>
+        <p className='footer__text'>Created with React power</p>
+    </div>
+  )
+}
+
+render (<MainApp/>, document.querySelector('.timer'));
+render (<FooterApp/>, document.querySelector('.footer'));
