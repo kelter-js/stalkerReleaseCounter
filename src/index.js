64eea70bb7c88eb346a22efdd23b56497f1d93b1
kelter-js <spawnkillerqw@gmail.com>
2021-08-28 14:43:33 +0400

    Добавляет ErrorBoundary для отладки ошибок в компонентах.

--- src/index.js
+++ src/index.js
@@ -10,4 +10,34 @@ const pageLanguageHandler = new PageLanguage (
   Constants.englishVocabulary
 );
 
-render (<App releaseDate = {Constants.releaseDate} pageLanguageHandler = {pageLanguageHandler.changeLanguage}/>, document.querySelector('.root'));
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(e) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      const rootContainer = document.querySelector('.root');
+      rootContainer.classList.add('errorContainer')
+      return (
+        <h1 className = 'errorMessage'>
+          Что-то пошло не так!
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+render (
+  <ErrorBoundary>
+    <App releaseDate = {Constants.releaseDate} pageLanguageHandler = {pageLanguageHandler.changeLanguage}/>
+  </ErrorBoundary>,
+  document.querySelector('.root')
+);
